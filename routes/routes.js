6217af4581207564a9ebb8cbf809a9bbf50a4f16
Kay-max-stack <67821926+Kay-max-stack@users.noreply.github.com>
2020-10-19 20:50:03 +0200

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express();
 const mongoDbController = require('../controller/dbController');
 
-//routen müßen noch angepasst werden. Delete sowie Detail ansicht gehen nicht mehr richtig aufgrund von sytels.css
+
 router.get('/home', mongoDbController.displayAllEntries);
 router.get('/home/:id', mongoDbController.displaySingleElementDetails);
 router.post('/home', mongoDbController.createNewPost);
